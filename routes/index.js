8a26edbc1df453b61d52c74901b0d613c499be7c
reina-duf <reina.dufrene@gmail.com>
2023-12-03 23:08:43 -0700

    adds oauth, adds conditional views on the index page for signed in or not

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cruise Journal Home' });
+  res.render('index', { title: 'Cruise Journal Log-In Page' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: "select_account"
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
+});
+
 
 module.exports = router;
