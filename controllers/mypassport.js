988f9d029f3e138740418acb6a98117445c8c794
reina-duf <reina.dufrene@gmail.com>
2023-12-08 18:34:09 -0700

    adds some styling to my passport

--- controllers/mypassport.js
+++ controllers/mypassport.js
@@ -6,12 +6,29 @@ module.exports = {
 
 async function index(req, res) {
   const userCruises = await Cruise.find({ user: req.user._id });
+  let memberStatus = await getMemberStatus(req)
+
+
+  res.render('mypassport', { userCruises, memberStatus, title: "My Passport" })
   
 
 
-  res.render('mypassport', { userCruises, title: "My Passport" })
+}
+
+async function getMemberStatus(req,res) {
+  const userCruises = await Cruise.find({ user: req.user._id });
+  let memberStatus;
   
+  if (userCruises.length >= 11) {
+    memberStatus = 'Sea Maestro (11+ cruises)'
+  } else if (userCruises.length >= 7 && userCruises.length <= 10) {
+    memberStatus = 'Cruise Connoisseur (7-10 cruises taken)'
+  } else if (userCruises.length >= 4 && userCruises.length <= 6) {
+    memberStatus = 'Nautical Explorer (4-6 cruises taken)'
+  } else if (userCruises.length < 3) {
+    memberStatus = 'Cruise Apprentice (0-3 cruises taken)'
+  }
 
-
+  return memberStatus;
 }
 
